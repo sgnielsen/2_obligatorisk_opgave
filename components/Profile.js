106bfabe3bf8e13f1bb00dfaf9ac59e6e9d21d7c
sgnielsen <sgnielsen11@gmail.com>
2023-10-08 16:48:29 +0200

    add to github

---
+++ components/Profile.js
@@ -0,0 +1,21 @@
+// Importerer nødvendige moduler
+import React, {useState} from 'react'
+import {Text, TextInput, View} from 'react-native'
+
+
+// Udsnit af hvordan profile skal se ud i fremtiden
+const Profile = () => {
+
+    const [inputValue, setInputValue] = useState("")
+
+    return(
+        <View>
+            <View style={{margin:10}}>
+                <Text>Profil data skal være her...</Text>
+            </View>
+
+        </View>
+    )
+}
+
+export default Profile;
